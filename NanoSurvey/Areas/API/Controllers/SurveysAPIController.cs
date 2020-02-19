fb7ecd9059fc7b52abec6450844c3078a862a9a5
Alexander Gluschenko <qugluschenko@gmail.com>
2020-02-20 02:06:43 +0300

    [add] MVC app

---
+++ NanoSurvey/Areas/API/Controllers/SurveysAPIController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NanoSurvey.Models;
+using NanoSurvey.Common;
+using NanoSurvey.Common.Data;
+using NanoSurvey.API;
+using NanoSurvey.Areas.API.Models;
+
+namespace NanoSurvey.Areas.API.Controllers
+{
+    [Route("api/surveys")]
+    public class SurveysAPIController : APIControllerBase<SurveysAPIController>
+    {
+        readonly SurveyDatabaseContext databaseContext;
+        
+        public SurveysAPIController(SurveyDatabaseContext context, ILogger<SurveysAPIController> logger) : base(logger)
+        {
+            databaseContext = context;
+        }
+
+        [Route("getList")]
+        public IActionResult GetList(int count, int offset) 
+        {
+            if (count > Limits.MaxSurveyCountPerRequest)
+                return JsonError($"Count is too big (max: {Limits.MaxSurveyCountPerRequest})");
+
+            return JsonResponce(databaseContext.Surveys.GetList(count, offset));
+        }
+
+        [Route("get")]
+        public IActionResult Get(int id)
+        {
+            if (id <= 0)
+                return JsonError("ID must be a positive number");
+
+            var result = databaseContext.Surveys.GetByID(id);
+            return result != null ? 
+                JsonResponce(result) : 
+                JsonError("This object isn't available");
+        }
+
+        [Route("saveResults")]
+        public IActionResult SaveResults(ServeySaveResultsModel model)
+        {
+            if (!ModelState.IsValid)
+                return JsonError(model);
+
+            return JsonResponce("OK");
+        }
+    }
+}
---
+++ NanoSurvey/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.WebEncoders;
+using NanoSurvey.Common;
+
+namespace NanoSurvey
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            string connection = Configuration.GetConnectionString("Main");
+            services.AddDbContext<SurveyDatabaseContext>(options => options.UseSqlServer(connection));
+
+            // Превращаем всякие &#345; в unicode
+            services.Configure<WebEncoderOptions>(options =>
+            {
+                options.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.All);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(name: "area", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+            });
+
+
+        }
+    }
+}
