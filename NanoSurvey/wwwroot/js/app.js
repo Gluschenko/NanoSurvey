8a5b74d14ae4315eafccae24bf584449a04cef19
Alexander Gluschenko <qugluschenko@gmail.com>
2020-02-21 00:12:15 +0300

    [feat] update front

--- NanoSurvey/wwwroot/js/app.js
+++ NanoSurvey/wwwroot/js/app.js
@@ -4,15 +4,36 @@
     },
 };
 
-const Surveys = {
-    getList: function (count, offset) {
-
+const SurveysAPI = {
+    getList: function (count, offset, response) {
+        API.CallMethod("surveys/getList", { count: count, offset: offset }, function (data) {
+            if (data.response) {
+                response(data.response);
+            }
+            else {
+                alert(data.error.error_msg);
+            }
+        });
     },
-    get: function (id) {
-
+    get: function (id, response) {
+        API.CallMethod("surveys/get", { id: id }, function (data) {
+            if (data.response) {
+                response(data.response);
+            }
+            else {
+                alert(data.error.error_msg);
+            }
+        });
     },
-    saveResults: function (model) {
-
+    saveResults: function (model, response) {
+        API.CallMethod("surveys/saveResults", {}, function (data) {
+            if (data.response) {
+                response(data.response);
+            }
+            else {
+                alert(data.error.error_msg);
+            }
+        });
     },
 };
 
@@ -25,5 +46,31 @@ const API = {
             response(data);
         };
         AJAX.Post(params, action, url);
+    },
+};
+
+$$ = {
+    body: function () { return document.body; },
+    create: function (parent, tag, classList) {
+        classList = classList || "";
+        tag = tag || "div";
+
+        let e = document.createElement(tag);
+        if (classList !== "") e.classList = classList;
+        parent.appendChild(e);
+        return e;
+    },
+    ready: function (func) { document.addEventListener("DOMContentLoaded", func); },
+    find: function (id) { return document.getElementById(id); },
+    select: function (selector) { return document.querySelector(selector); },
+    selectAll: function (selector) { return document.querySelectorAll(selector); },
+    isObject: function (obj) {
+        let type = typeof obj;
+        return type === 'function' || type === 'object' && !!obj;
+    },
+    clear: function (obj) {
+        while (obj.firstChild) {
+            obj.removeChild(obj.firstChild);
+        }
     },
 };
